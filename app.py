c5633523495537a6d7cd2be56bfbfdac510b93d7
poornesh <ks.poornesh@gmail.com>
2025-07-11 17:58:06 +0530

    Update Flask home route to trigger deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Flask App deployed via CI/CD on Azure!"
+    return "🚀 Hello from Flask App deployed viaa CI/CD on Azure!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
